0f0b226a2e5cff522cee3658e5db43015f8923bb
Kiril Chuchev <54711535+KirilChuchev@users.noreply.github.com>
2019-09-24 13:52:07 +0300

    Add files via upload

---
+++ 2. Change List/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Change_List
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> integers = (Console.ReadLine()).Split().Select(int.Parse).ToList();
+
+			string input = string.Empty;
+			
+			while ((input=Console.ReadLine()) != "end")
+			{
+				string[] commands = input.Split().ToArray();
+
+				switch (commands[0])
+				{
+					case "Delete":
+						{
+							
+							for (int i = 0; i < integers.Count; i++)
+							{
+								if (integers[i] == int.Parse(commands[1]))
+								{
+									integers.Remove(integers[i--]);
+
+								}
+							}
+						}
+						break;
+					case "Insert":
+						{
+							integers.Insert(int.Parse(commands[2]), int.Parse(commands[1]));
+						}
+						break;
+					
+				}
+			}
+
+			Console.WriteLine(string.Join(" ", integers));
+		}
+	}
+}
---
+++ 3. House Party - 1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._House_Party
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int commandsCount = int.Parse(Console.ReadLine());
+
+			List<string> guests = new List<string>();
+
+			for (int i = 0; i < commandsCount; i++)
+			{
+				
+				List<string> command = (Console.ReadLine()).Split().ToList();
+
+				if (!(command.Contains("not")))
+				{
+					if (!(guests.Contains(command[0])))
+					{
+						guests.Add(command[0]);
+
+						
+					}
+					else
+					{
+						guests.Add(command[0]);
+						//Console.WriteLine($"{command[0]} is already in the list!");
+
+						
+					}
+				}
+				else
+				{
+					for (int j = 0; j < guests.Count; j++)
+					{
+						if (guests[j] == command[0])
+						{
+							guests.Remove($"{command[0]}");
+							//guests.RemoveAt(j);
+						}
+					}
+				}
+			}
+
+			foreach (var item in guests)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
